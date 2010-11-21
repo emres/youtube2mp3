c010057791d7c2bde55e7e23ef96dbe196f2f274
Emre Sevinç <emre.sevinc@gmail.com>
2010-11-21 21:07:37 +0100

    Minor code simplification by decreasing redundancy.

--- youtube2mp3.sh
+++ youtube2mp3.sh
@@ -9,18 +9,19 @@ if [[ $return_code -eq 0 ]]; then
 		video_id=$(echo $video_id | cut -d'&' -f1)
 		video_title="$(youtube-dl --get-title $address)"
 		youtube-dl $address
+
 		if [ -e $video_id.flv ]; then
-			ffmpeg -i $video_id.flv "$video_title".wav
-			lame "$video_title".wav "$video_title".mp3
-			rm $video_id.flv
+			ext="flv"
 		fi
 
 		if [ -e $video_id.mp4 ]; then
-			ffmpeg -i $video_id.mp4 "$video_title".wav
-			lame "$video_title".wav "$video_title".mp3
-			rm $video_id.mp4
+			ext="mp4"
 		fi
-		rm "$video_title".wav
+
+		ffmpeg -i $video_id.$ext "$video_title".wav
+		lame "$video_title".wav "$video_title".mp3
+		rm $video_id.$ext "$video_title".wav
+
 	    zenity --width=260 --height=130 --title "YouTube MP3 Extractor" --info --text "Your MP3 file is ready."
 	else
 		zenity --error --text "Sorry but the system encountered a problem. Please check your YouTube address and try again later..."
